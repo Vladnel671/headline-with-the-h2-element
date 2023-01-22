7b679c20115b6de3cae35e09d587d22e18e681fa
vlad <vnelipovich63@gmail.com>
2023-01-22 12:54:22 +0300

    who likes it

--- jscodewars/who likes it.js
+++ jscodewars/who likes it.js
@@ -1,19 +1,29 @@
 let names = ['Alex', 'Jacob', 'Mark', 'Max'];
 
 function likes(names) {
+    if (names.length < 1) {
+        return "no one likes this"
+    }
     for (let i = 0; i < names.length; i++) {
-        if (names.length < 0) {
-            return "no one likes this"
-        } else if (names.length = 0) {
-            return names[0] + " likes this"
-        } else if (names.length = 1) {
-            return names[0] + "and" + names[1] + " like this"
-        } else if (names.length = 2) {
-            return names[0] + ", " + names[1] + " and " + names[2] + " like this"
-        } else if (names.length >= 3) {
-            return names[0] + ", " + names[1] + " and " + names.length - 1 + " others like this"
+
+        if (names.length == 1) {
+            return names[i] + " likes this"
+        }
+
+        if (names.length == 2) {
+            return names[i] + " and " + names[i + 1] + " like this"
+        }
+
+        if (names.length == 3) {
+            return names[i] + ", " + names[i + 1] + " and " + names[i + 2] + " like this"
+        }
+
+        if (names.length >= 4) {
+            let count = names.length;
+            count -= 2;
+            return names[i] + ", " + names[i + 1] + " and " + count + " others like this"
         }
     }
 }
 
-console.log(likes(names[0]))
+console.log(likes(names))
