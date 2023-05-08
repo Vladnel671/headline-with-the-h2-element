04d25d0cdef264890c3bce5451c8181aff45128e
vlad <vnelipovich63@gmail.com>
2023-05-08 23:54:57 +0300

    Find Multiples of a Number

---
+++ jscodewars/Find Multiples of a Number.js
@@ -0,0 +1,9 @@
+function findMultiples(integer, limit) {
+    let multiples = [];
+
+    for (let i = integer; i <= limit; i = i + integer) {
+        multiples.push(i);
+
+    }
+    return multiples
+}
